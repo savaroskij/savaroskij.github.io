6cbe961905d464c87e2653c791f1b1200548599c
Matteo Savatteri <matteo.savatteri@bruttocarattere.org>
2020-12-24 15:11:11 +0100

    Change magenta to red and make blink period shorter

--- blink.js
+++ blink.js
@@ -1,8 +1,8 @@
 (function() {
   var h1 = document.getElementById('h1');
-  var color = 'magenta';
+  var color = 'red';
   setInterval(function() {
     h1.style.color = color;
-    color = (color === 'magenta') ? 'green' : 'magenta';
-  }, 500);
+    color = (color === 'red') ? 'green' : 'red';
+  }, 100);
 })();
